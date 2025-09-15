3c5b91178deadf2d0b83007f86f5b43a8599fbf2
Loic <loic.noro@epitech.eu>
2025-09-15 17:09:33 +0200

    Deleting history

---
+++ configs/mysql_config.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class MysqlConfig {
+    const SERVEUR = '';
+    const BASE = '';
+    const UTILISATEUR = '';
+    const MOT_DE_PASSE = '';
+//    const SERVEUR = 'localhost';
+//    const BASE = 'stage';
+//    const UTILISATEUR = 'root';
+//    const MOT_DE_PASSE = '';
+}
+
+?>
